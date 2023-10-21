3be1f95b26fd8276c9db68cada2a359fbbb55a09
karshdev <sharmaaakarsh332@gmail.com>
2023-10-21 21:39:37 +0530

    starting from 0

--- app/api/users/route.js
+++
@@ -1,8 +0,0 @@
-import { ExampleTable, db } from '@/libs/drizzle';
-import { NextResponse } from 'next/server';
- 
-export async function GET(request) {
-  const client=await db.select().from(ExampleTable)
-  console.log("Client",client);
-return NextResponse.json(client)
-}
