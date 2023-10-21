c05ed4d23958d4c783d5c4e27e3df8f4877654bb
karshdev <sharmaaakarsh332@gmail.com>
2023-10-21 22:54:22 +0530

    added api's drizzle.js

---
+++ app/api/todo/[id]/route.js
@@ -0,0 +1,13 @@
+
+import { NextResponse } from 'next/server';
+ 
+export async function PUT(request) {
+    return NextResponse.json("PUT")
+ 
+}
+
+ 
+export async function DELETE(request) {
+    return NextResponse.json("DELETE")
+ 
+}
--- app/api/todo/route.js
+++ app/api/todo/route.js
@@ -1,7 +1,21 @@
 
 import { NextResponse } from 'next/server';
- 
+ import {db,todo} from '@/lib/drizzle'
 export async function GET(request) {
     return NextResponse.json("hello")
+ 
+}
+
+ 
+export async function POST(request) {
+  const {data}=await request.json()
+ 
+  const newTodo={
+  taskname:data,
+  isDone:false
+  }
+const insertDb=await db.insert(todo).values(newTodo).returning()
+console.log("insertDb",insertDb);
+    return NextResponse.json({message:"Success"})
  
 }
